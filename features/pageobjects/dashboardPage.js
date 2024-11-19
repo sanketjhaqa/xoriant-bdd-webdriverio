17c62d8dad41f5e3748d4997a79568567bc130ff
Sanket Jha <sanketjha@Sankets-MacBook-Pro.local>
2024-11-19 15:48:30 +0530

    comments added

--- features/pageobjects/dashboardPage.js
+++ features/pageobjects/dashboardPage.js
@@ -2,7 +2,11 @@ import { $, browser, driver } from '@wdio/globals'
 
 
 class DashboardPage
-{
+{   
+    // get Element named as PRODUCTS from dashboard page 
+    // finding element for IOS using IOS Class Chain
+    // for android using Xpath
+    
     get product(){
         if(browser.capabilities.platformName.toLowerCase()=='ios'){
             const selector = '**/XCUIElementTypeStaticText[`name == "PRODUCTS"`]';
--- features/pageobjects/loginPage.js
+++ features/pageobjects/loginPage.js
@@ -2,19 +2,23 @@ import { $ } from '@wdio/globals'
 
 class LoginPage {
 
-
+    // get username field using accessbility-id
     get userName(){
         return $('~test-Username')
     }
 
+     // get password field using accessbility-id
     get password(){
         return $('~test-Password')
     }
 
+    // get login btn using accessbility-id
     get loginBtn(){
         return $('~test-LOGIN')
     }
 
+
+    // entering login details and login into the dashboard
     async enterDetailsAndLogin(username,password){
        
         await this.userName.setValue(username);
